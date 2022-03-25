928960d371147076623c068ec0afa11fef731408
Hassan Chowdhury <hassan.chowdhury1@gmail.com>
2022-03-25 22:57:45 +0000

    feat: add layout styles to the form

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,7 +1,7 @@
 import { useForm, Controller } from "react-hook-form";
 import type { SubmitHandler } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { Button, TextField } from "@mui/material";
+import { Box, Button, TextField } from "@mui/material";
 
 import { schema } from "./Form.schema";
 import type { FormState } from "./Form.types";
@@ -27,21 +27,23 @@ export const Form = () => {
   return (
     <form>
       <p>form</p>
-      <Controller
-        control={control}
-        render={({ field }) => (
-          <TextField
-            {...field}
-            id={field.name}
-            error={!!errors.firstName}
-            helperText={errors.firstName ? errors.firstName?.message : ""}
-          />
-        )}
-        name="firstName"
-      />
-      <Button onClick={handleSubmit(onSubmit)} variant="contained">
-        Submit
-      </Button>
+      <Box sx={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
+        <Controller
+          control={control}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              id={field.name}
+              error={!!errors.firstName}
+              helperText={errors.firstName ? errors.firstName?.message : ""}
+            />
+          )}
+          name="firstName"
+        />
+        <Button onClick={handleSubmit(onSubmit)} variant="contained">
+          Submit
+        </Button>
+      </Box>
     </form>
   );
 };
